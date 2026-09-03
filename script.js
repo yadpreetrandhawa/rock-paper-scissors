24fd55ac3c12072c71190e5ae9cf8fe23bd635fe
Yadpreet Randhawa <295484453+yadpreetrandhawa@users.noreply.github.com>
2026-09-03 11:39:58 -0700

    Finish basic logic via prompting and logging

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
-console.log("Linked")
+let humanScore = 0;
+let computerScore = 0;
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function getComputerChoice() {
+  let choice = getRandomInt(3);
+  return choice;
+}
+
+function getHumanChoice() {
+  let choice = prompt("Enter a choice, [1: Rock, 2: Paper, 3: Scissors]");
+  return choice.toLowerCase();
+}
+
+
+function playRound(humanChoice, computerChoice) {
+  const options = ['rock', 'paper', 'scissors'];
+
+  humanMove = options[humanChoice];
+  computerMove = options[computerChoice];
+
+  if (humanChoice === computerChoice) {
+    console.log(`Tie. ${humanMove} vs ${computerMove}`);
+  } else if (
+    humanChoice == 0 && computerChoice == 2 ||
+    humanChoice == 1 && computerChoice == 0 ||
+    humanChoice == 2 && computerChoice == 1
+  ) {
+    console.log(`You win!. ${humanMove} vs ${computerMove}`);
+    humanScore++;
+  } else {
+    console.log(`You lose!. ${humanMove} vs ${computerMove}`);
+    computerScore++;
+  }
+  console.log(`Scoreboard: [Player: ${humanScore}] [Computer: ${computerScore}]`);
+}
+
+
+function playGame() {
+  for (let i = 0; i < 5; i++) {
+  const humanSelection = getHumanChoice();
+  const computerSelection = getComputerChoice();
+
+  playRound(humanSelection, computerSelection);
+  }
+}
+
+playGame();
